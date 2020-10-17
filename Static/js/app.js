d7fc6bfd28bba9e77542a54b6f45d066e7a39421
hauckc21 <hauck.chris@gmail.com>
2020-10-17 14:10:39 -0500

    nav bar

--- Static/js/app.js
+++ Static/js/app.js
@@ -1,11 +1,1 @@
 console.log("This is app.js");
-
-var button = d3.select("#pick-your-org");
-
-function handleClick() {
-    console.log("a button was clicked");
-
-    console.log(d3.event.target);
-}
-
-button.on("click", handleClick);
